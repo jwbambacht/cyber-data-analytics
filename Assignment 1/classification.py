f239d262a8ac8208fa4520f3aec18b73ae70996f
Joost <j.w.bambacht@student.tudelft.nl>
2020-05-10 18:02:40 +0200

    Change SMOTE to be only applied to train set in each fold

--- Assignment 1/classification.py
+++ Assignment 1/classification.py
@@ -11,11 +11,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve, plot_roc_curve, confusion_matrix
-from sklearn.model_selection import KFold 
 import random
-import imbalanced as imb
 import classification as clf
 
 sns.set_style("darkgrid")
@@ -26,10 +25,16 @@ def load_data(file_name):
 
 	return data
 
+def get_fraud(data):
+	return data.loc[data['simple_journal'] == 1]
+
+def get_non_fraud(data):
+	return data.loc[data['simple_journal'] == 0]
+
 def describe_data(data):
 	total_size = len(data)
-	fraud_size = len(imb.get_fraud(data))
-	non_fraud_size = len(imb.get_non_fraud(data))
+	fraud_size = len(clf.get_fraud(data))
+	non_fraud_size = len(clf.get_non_fraud(data))
 
 	fraud_ratio = round(fraud_size/len(data)*100,4)
 	non_fraud_ratio = round(non_fraud_size/len(data)*100,4)
@@ -95,40 +100,121 @@ def get_X_y(data):
 
 	return X,y
 
-def get_n_folds(n,X):
+def nearest_neighbor(data):
+	nn = NearestNeighbors(n_neighbors=5, metric='euclidean', algorithm='kd_tree').fit(data)
+	dist, index = nn.kneighbors(data)
+	return index
 
-	kf = KFold(n_splits=n,shuffle=True,random_state=13)
-	
-	return kf
+def smote_item(data):
+	indices = nearest_neighbor(data)
 
-def train_test(X,y,test_ratio):
-	X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_ratio,random_state=13)
-	
-	return X_train, X_test, y_train, y_test
+	result = []
 
-def classify(classifier, X_train, y_train, X_test):
-	if classifier == "KNN":
-		clf = KNeighborsClassifier(n_neighbors=3)
-	elif classifier == "RandomForest":
-		clf = RandomForestClassifier(random_state=0, n_estimators=100)
-	elif classifier == "NaiveBayes":
-		clf = GaussianNB()
-	elif classifier == "SupportVectorMachines":
-		clf = SVC(probability=True)
-	elif classifier == "LogisticRegression":
-		clf = LogisticRegression()
-	elif classifier == "DecisionTree":
-		clf = DecisionTreeClassifier()
-	elif classifier == "LinearDiscriminantAnalysis":
-		clf = LinearDiscriminantAnalysis()
-	elif classifier == "QuadraticDiscriminantAnalysis":
-		clf = QuadraticDiscriminantAnalysis()
+	for m in range(len(indices)):
+		t = data[indices[m]]
+		new_t = pd.DataFrame(t)
+		result.append([])
 
-	clf.fit(X_train,y_train)
-	predicted = clf.predict(X_test)
-	predicted_proba = clf.predict_proba(X_test)
+		for j in range(len(new_t.columns)):
+			result[m].append(random.choice(new_t[j]))
 
-	return clf, predicted, predicted_proba
+	return result
+
+def smote_dataset(X_UNSMOTEd, y_UNSMOTEd,N):
+
+	X_UNSMOTEd = np.array(X_UNSMOTEd)
+	y_UNSMOTEd = np.array(y_UNSMOTEd)
+
+	X_SMOTEd = X_UNSMOTEd
+	y_SMOTEd = y_UNSMOTEd
+
+	for i in range(N):
+		unique, counts = np.unique(y_UNSMOTEd,return_counts=True)
+		minority_shape = dict(zip(unique, counts))[1]
+
+		x = np.ones((minority_shape, X_UNSMOTEd.shape[1]))
+		x = [X_UNSMOTEd[i] for i, v in enumerate(y_UNSMOTEd) if v == 1.0]
+		x = np.array(x)
+
+		X_sampled = smote_item(x)
+		X_SMOTEd = np.concatenate((X_SMOTEd, X_sampled), axis=0)
+
+		y_sampled = np.ones(minority_shape)
+		y_SMOTEd = np.concatenate((y_SMOTEd, y_sampled), axis=0)
+
+	return X_SMOTEd, y_SMOTEd
+
+def set_threshold(y_prob, threshold):
+	y_pred = y_prob[:,1]
+	for i in range(len(y_prob)):
+		if(y_prob[i,1] >= threshold):
+			y_pred[i] = 1
+		else:
+			y_pred[i] = 0
+
+	return y_pred
+
+def classify_knn(data, N):
+	X, y = clf.get_X_y(data)
+	kf = StratifiedKFold(n_splits=10, shuffle=True,random_state=13)
+	# kf = KFold(n_splits=10,random_state=33)
+
+	classifier = KNeighborsClassifier(n_neighbors=3)
+
+	tp_total = 0
+	tn_total = 0
+	fp_total = 0
+	fn_total = 0
+
+	for train_index, test_index in kf.split(X, y):
+	# for train_index, test_index in kf.split(X):
+		X_train, X_test, y_train, y_test = X[train_index], X[test_index], y[train_index], y[test_index]
+
+		X_train, y_train = clf.smote_dataset(X_train, y_train, N)
+
+		classifier.fit(X_train,y_train)
+		y_prob = classifier.predict_proba(X_test)
+		y_pred = clf.set_threshold(y_prob,0.51)
+
+		tn, fp, fn, tp = confusion_matrix(y_test,y_pred).ravel()
+		tp_total += tp
+		tn_total += tn
+		fp_total += fp
+		fn_total += fn
+
+	print(f"TruePositives: {tp_total}")
+	print(f"TrueNegatives: {tn_total}")
+	print(f"FalsePositives: {fp_total}")
+	print(f"FalseNegatives: {fn_total}")
+
+def classify_random_forest(classifierName, data, N):
+	X, y = clf.get_X_y(data)
+	X, y = clf.smote_dataset(X, y, N)
+	kf = KFold(n_splits=10,shuffle=True,random_state=13)
+
+	classifier = RandomForestClassifier(random_state=0, n_estimators=100)
+
+	tp_total = 0
+	tn_total = 0
+	fp_total = 0
+	fn_total = 0
+
+	for train_index, test_index in kf.split(X):
+		X_train, X_test, y_train, y_test = X[train_index], X[test_index], y[train_index], y[test_index]
+
+		classifier.fit(X_train,y_train)
+		y_pred = classifier.predict(X_test)
+
+		tn, fp, fn, tp = confusion_matrix(y_test,y_pred).ravel()
+		tp_total += tp
+		tn_total += tn
+		fp_total += fp
+		fn_total += fn
+
+	print(f"TruePositives: {tp_total}")
+	print(f"TrueNegatives: {tn_total}")
+	print(f"FalsePositives: {fp_total}")
+	print(f"FalseNegatives: {fn_total}")
 
 def get_performance(clf, predicted, y_test,name):
 
