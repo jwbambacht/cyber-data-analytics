f41937205b95230fa98c00a931fa5effcd3349ff
Lennart Overdevest <lennart_basketbal@msn.com>
2020-05-26 22:56:08 +0200

    Fix code

--- Assignment 2/anomaly_detection.py
+++ Assignment 2/anomaly_detection.py
@@ -6,16 +6,21 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import LocalOutlierFactor
 
-def compute_lof(data, signals, max_neighbours):  
+def compute_lof(data, signals, max_neighbours, test=0):  
     # Initialize data     
-    lof_curvs = [ [] for _ in range(len(signals)) ]     
-    neighbour_array = range(1,max_neighbours)
+    lof_curvs = [ [] for _ in range(len(signals)) ]
+    if test == 0:
+        neighbour_array = range(1,max_neighbours)
+    else:
+        neighbour_array = range(max_neighbours, max_neighbours + 1)
     count_signal = 0
     lof_data = [ [] for _ in range(len(data['P_J306'])) ]
     
-    # Compute LOF curve for each signal     
+    # Compute LOF curve for each signal
+    outliers = []
     for signal in signals:
         pref_lof = []
+        outliers_index = {}
         data[signal] = np.random.permutation(data[signal].values)
         for i in range(len(data[signal])):
             lof_data[i] = [data[signal][i]]
@@ -26,14 +31,21 @@ def compute_lof(data, signals, max_neighbours):
             clf = LocalOutlierFactor(n_neighbors=n)
             clf.fit_predict(lof_data)
             res = clf.negative_outlier_factor_
+            count = 0
             for r in res:
-                # Important threshold to define when a data point is a anomaly        
-                if r < -1.5:
+                # Important threshold to define when a data point is a anomaly   
+                if test == 1:
+                    outliers_index[count] = r
+                if r < -1.5 and test == 0:
                     outliers_detected += 1
+                count += 1
             pref_lof.append(outliers_detected)
+        outliers.append(outliers_index)
         # Save data         
         lof_curvs[count_signal] = pref_lof
         count_signal += 1
+    if test == 1:
+        return outliers
     return lof_curvs
 
 def plot_lof(lof_curvs, signals, max_neighbours, title):
