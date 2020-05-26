e758769bdd026881b0848ac1f104c0c79fffceea
Joost <j.w.bambacht@student.tudelft.nl>
2020-05-26 14:27:48 +0200

    PCA done

--- Assignment 2/pca_helper.py
+++ Assignment 2/pca_helper.py
@@ -8,6 +8,7 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 from scipy import stats
 from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 sns.set_style("darkgrid")
 
@@ -18,14 +19,25 @@ def load_data(file_name):
 	return data
 
 # Preprocess the data set
-def pre_process(data):
+def pre_process(data,train_data_2=False):
 
 	# Remove the datetime and truth label columns from dataset
 	tdata = data.copy()
 
 	tdata = tdata.rename(columns = {' ATT_FLAG':'ATT_FLAG'})
 
-	labels = tdata["ATT_FLAG"]
+	if train_data_2 == True:
+		start = [1727, 2027, 2337, 2827, 3497, 3727, 3927]
+		end = [1776, 2050, 2396, 2920, 3556, 3820, 4036]
+
+		labels = np.zeros(len(data))
+
+		for i in range(7):
+			x = np.arange(start[i],end[i]+1,1)
+			for row in x:
+				labels[row] = 1
+	else:
+		labels = np.zeros(len(data))
 
 	tdata.drop(columns=["DATETIME","ATT_FLAG"],axis=1,inplace=True)
 
@@ -46,16 +58,16 @@ def pre_process(data):
 	return tdata, labels
 
 def number_of_components_plot(pca, n_features, threshold):
-	fig, ax = plt.subplots(figsize=(10,5))
+	fig, ax = plt.subplots(figsize=(10,2))
 	x = np.arange(1, n_features+1, step=1)
 	y = np.cumsum(pca.explained_variance_ratio_)
 
 	plt.ylim(0.18,1.1)
 	plt.plot(x, y, marker='o', linestyle='--', color='b')
 
-	plt.xlabel('# Components', fontsize=14)
+	plt.xlabel('# Components', fontsize=12)
 	plt.xticks(np.arange(0, n_features+1, step=1))
-	plt.ylabel('Cumulative variance (%)', fontsize=14)
+	plt.ylabel('Cumulative variance (%)', fontsize=12)
 	plt.title('Number of components', fontsize=18)
 
 	plt.axhline(y=0.97, color='r', linestyle='-')
@@ -74,6 +86,78 @@ def number_of_components_plot(pca, n_features, threshold):
 			break;
 
 	return pca.components_
+
+def predict_labels(residual, threshold):
+	labels_pred = np.zeros(len(residual))
+
+	for i in range(len(residual)):
+		if residual[i] >= threshold:
+			labels_pred[i] = 1
+
+	return labels_pred
+
+def performance(labels, labels_pred):
+	tn, fp, fn, tp = confusion_matrix(labels, labels_pred).ravel()
+
+	print("Accuracy:", accuracy_score(labels,labels_pred))
+	print("Precision:", precision_score(labels,labels_pred))
+	print("Recall:", recall_score(labels,labels_pred))
+	print("TP:", tp)
+	print("FP:", fp)
+	print("FN:", fn)
+	print("TN:", tn)
+
+def plot_threshold_search(residual, labels):
+	
+	thresholds = list()
+	accuracies = list()
+	precisions = list()
+	recalls = list()
+	tps = list()
+	fps = list()
+
+	mean = np.mean(residual)
+	diff = (np.max(residual)-mean)/0.001
+
+	for i in range(0,int(diff)):
+		threshold = mean+i*0.001
+		thresholds.append(threshold)
+		labels_pred = pca_helper.predict_labels(residual,threshold)
+
+		tn, fp, fn, tp = confusion_matrix(labels, labels_pred).ravel()
+
+		accuracies.append(accuracy_score(labels,labels_pred))
+		precisions.append(precision_score(labels,labels_pred))
+		recalls.append(recall_score(labels,labels_pred))
+		tps.append(tp)
+		fps.append(fp)
+
+	results = pd.DataFrame(thresholds, columns=["Threshold"])
+	results['Accuracy'] = accuracies
+	results['Precision'] = precisions
+	results['Recall'] = recalls
+	results['TP'] = tps
+	results['FP'] = fps
+	results['TP_NORM'] = tps/np.max(tps)
+	results['FP_NORM'] = fps/np.max(fps)
+
+	f,ax = plt.subplots(figsize=(22,5))
+	plt.plot(results["Accuracy"], color='red',label='Accuracy')
+	plt.plot(results["Precision"], color='blue', label='Precision')
+	plt.plot(results["Recall"], color='green', label='Recall')
+	plt.plot(results["TP_NORM"], color='yellow',label='Normalized TP')
+	plt.plot(results["FP_NORM"], color='magenta', label='Normalized FP')
+	plt.axvline(448, label='Optimal threshold')
+	ax.legend(loc='upper right')
+	ax.set_title("Performance metrics per threshold", size=14)
+	ax.set_ylabel("Results", size=12)
+	plt.show()
+
+def plot_anomaly_range(residual, start, end,type):
+	fig, ax = plt.subplots(figsize=(22,3))
+	plt.plot(residual[start:end])
+	ax.set_title("Part of the residual plot, which is labeled and detected to be a "+type+" anomaly",size=14)
+	plt.show()
 
 
 
