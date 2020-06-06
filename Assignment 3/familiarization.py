5f2907b3690794b1e1cf49766ae0d3ab04241233
Joost <j.w.bambacht@student.tudelft.nl>
2020-06-06 22:21:20 +0200

    Finish familiarization task

--- Assignment 3/familiarization.py
+++ Assignment 3/familiarization.py
@@ -6,6 +6,7 @@ import familiarization as fam
 from sklearn.cluster import KMeans
 from yellowbrick.cluster import KElbowVisualizer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import KBinsDiscretizer
 
 sns.set_style("darkgrid")
 
@@ -40,14 +41,16 @@ def encode_feature(data):
 	return data
 
 def select_non_infected_host(data):
-	infected_hosts_addr = ["147.32.84.165","147.32.84.191","147.32.84.192","147.32.84.193","147.32.84.204","147.32.84.205","147.32.84.206","147.32.84.207","147.32.84.208","147.32.84.209"]
+	data = data.loc[data["Infected"] == 0]
+
+	non_infected_hosts = data.SourceAddress.unique()
 
 	counts = list()
 
-	for host in infected_hosts_addr:
-		counts.append(len(data.loc[data["SourceAddress"] != host]))
+	for host in non_infected_hosts:
+		counts.append(len(data.loc[data["SourceAddress"] == host]))
 
-	return infected_hosts_addr[counts.index(max(counts))]
+	return non_infected_hosts[counts.index(max(counts))]
 
 
 def select_infected_host(data):
@@ -65,9 +68,35 @@ def elbow(data, col):
 
 	reshaped_data = np.array(data[col]).reshape(-1,1)
 
-	max_clusters = min(10,data[col].nunique())
+	if col == "Protocol":
+		k = (2,6)
+	else:
+		k = (2,11)
 
 	model = KMeans()
-	visualizer = KElbowVisualizer(model)
+	visualizer = KElbowVisualizer(model, k=k)
 	visualizer.fit(reshaped_data)
 	visualizer.show()
+
+def discretize_feature(data, feature, nbin, strategy):
+	X = np.array(data[feature]).reshape(-1,1)
+	enc = KBinsDiscretizer(n_bins=nbin, encode="ordinal", strategy=strategy)
+	Xt = enc.fit_transform(X)
+	binsedges = enc.bin_edges_[0]
+
+	# needs to be done otherwise the first bucket doesnt contain any data 
+	binsedges[0] = binsedges[0] - 0.0000001
+	new_labels = range(0, len(binsedges)-1)
+
+	discretized = pd.cut(np.array(data[feature]), bins=binsedges, labels=new_labels)
+
+	return discretized, binsedges
+
+def concatenate_columns(column_one, column_two):
+	# Concatenates the values of two columns
+	return str(int(column_one))+""+str(int(column_two))
+
+
+
+
+
