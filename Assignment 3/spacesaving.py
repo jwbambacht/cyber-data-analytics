4ceec91ee06c4c5c22e7fcf7de5246b943a7b362
Lennart Overdevest <lennart_basketbal@msn.com>
2020-06-09 19:25:45 +0200

    Structured code better

--- Assignment 3/spacesaving.py
+++ Assignment 3/spacesaving.py
@@ -49,15 +49,6 @@ def SpaceSaving(data, k):
 
     # Sort the frequency in descending order     
     sorted_counter = {k: v for k, v in sorted(counter_list.items(), key=lambda item: item[1], reverse=True)}
-    
-#     discretized_counter = {}
-#     for key, value in sorted_counter.items():
-#         current_encoding = [int(key[0]), int(key[1]), int(key[2])]
-#         # Find old encoding         
-#         old_encoding = le.inverse_transform(current_encoding)
-#         new_key = ','.join(old_encoding)
-#         # Replace new encoding with old encoding, to understand the meaning of the n-grams
-#         discretized_counter[new_key] = value
     return sorted_counter 
 
 def ActualFrequency(data): 
@@ -82,36 +73,4 @@ def ActualFrequency(data):
          
     # Sort the frequency in descending order     
     sorted_counter = {k: v for k, v in sorted(freq_dict.items(), key=lambda item: item[1], reverse=True)}
-    
-#     freq_counter = {}
-#     for key, value in sorted_counter.items():
-#         current_encoding = [int(key[0]), int(key[1]), int(key[2])]
-#         # Find old encoding         
-#         old_encoding = le.inverse_transform(current_encoding)
-#         new_key = ','.join(old_encoding)
-#         # Replace new encoding with old encoding, to understand the meaning of the n-grams
-#         freq_counter[new_key] = value
-    return sorted_counter 
-
-
-
-
-
-# def plot_ngram_frequency(data, counter_size):
-#     # Create dictionary only containing top 10 most frequent n-grams(dictionary already sorted in decreasing value order)
-#     top_ten_items = data.items()
-#     top_ten_items = list(top_ten_items)[:10]
-#     top_ten = {}
-#     for i in top_ten_items:
-#         top_ten[i[0]] = i[1]
-
-
-#     # Create dataframe for the plot, where x-axes is ngram profile and y-axes is the frequency count     
-#     plot_df = pd.DataFrame()
-#     plot_df['ngram'] = top_ten.keys()
-#     plot_df['frequency'] = top_ten.values()
-#     fig, ax = plt.subplots(ncols=1,figsize=(22,10))
-#     sns.barplot(x="ngram", data=plot_df, y='frequency')
-#     ax.set_title("10 most frequent 3-grams using " + str(counter_size) + " counters - Non Infected Host: Protocol-Duration feature", size=16)
-#     plt.xticks(rotation=90)
-#     plt.show()
+    return sorted_counter
